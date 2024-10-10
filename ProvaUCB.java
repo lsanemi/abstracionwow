7716bc22a75ddb71c7da7d7cce54b01c5077cfbd
lsanemi <127852383+lsanemi@users.noreply.github.com>
2024-10-10 11:08:16 -0300

    Add files via upload

---
+++ ProvaUCB.java
@@ -0,0 +1,12 @@
+package com.mycompany.abstracao;
+
+public class ProvaUCB extends ProvaUniversidade {
+
+    public ProvaUCB(double notaAV1, double notaAV2) {
+        super(notaAV1, notaAV2);
+    }
+
+    public boolean aprovado() {
+        return calcularMedia() >= 7;
+    }
+}
